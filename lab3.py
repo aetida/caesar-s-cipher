fc0663b0c6374f059bfbfba80089158061c3f948
dmitry <113114524+aetida@users.noreply.github.com>
2022-10-02 17:27:47 +0500

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,17 @@
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+msg_input = input('Введите сообщение для шифровки: ').lower()
+step = (int(input('Введите шаг шифровки от 1 до 33: ')))
+msg_output ='' 
+
+for i in msg_input:
+    letter_place = alphabet.find(i)
+    new_letter_place = letter_place + step
+    if i in alphabet:
+        msg_output += alphabet[new_letter_place % len(alphabet)]
+    else:
+        msg_output += i 
+
+print(msg_output)
+
+ 
