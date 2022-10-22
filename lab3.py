21e073028ace6c9abe674f823c974509b93e79d9
dmitry <113114524+aetida@users.noreply.github.com>
2022-10-22 11:14:39 +0500

    added a decoder

--- lab3.py
+++ lab3.py
@@ -1,8 +1,14 @@
 alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 
+select_act = int(input('Выберите действие \n [1] Зашифровать сообщение \n [2] Расшифровать сообщение \n'))
 msg_input = input('Введите сообщение для шифровки на кириллице: ').lower()
 step = (int(input('Введите шаг шифровки от 1 до 33: ')))
 msg_output ='' 
+
+if select_act == 2:
+    step = step*(-1)
+else:
+    pass
 
 for i in msg_input:
     letter_place = alphabet.find(i)
